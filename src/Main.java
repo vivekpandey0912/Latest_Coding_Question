12d51c202bf5cd4e0949f3e670201baf57f968d6
Vivek pandey <vp1446392@gmail.com>
2023-07-11 16:53:31 +0530

    Binary Search Completed

--- src/Main.java
+++ src/Main.java
@@ -1052,6 +1052,40 @@ class LinerSearch
     }
 }
 
+class BinarySearch {
+    public static int binarySearch(int[] array, int target) {
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (array[mid] == target) {
+                return mid; // Found the target at index mid
+            } else if (array[mid] < target) {
+                left = mid + 1; // Target is in the right half
+            } else {
+                right = mid - 1; // Target is in the left half
+            }
+        }
+
+        return -1; // Target not found
+    }
+
+    public static void main(String[] args) {
+        int[] array = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+        int target = 12;
+
+        int index = binarySearch(array, target);
+
+        if (index != -1) {
+            System.out.println("Target found at index " + index);
+        } else {
+            System.out.println("Target not found in the array.");
+        }
+    }
+}
+
 
 
 
