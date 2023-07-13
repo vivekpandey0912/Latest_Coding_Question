1b81da8ea0197cce6837da79d06ec49402b4ea43
Vivek pandey <vp1446392@gmail.com>
2023-07-13 16:47:38 +0530

    creating Singlton Class

--- src/Main.java
+++ src/Main.java
@@ -1164,7 +1164,45 @@ class PrintTotalCharInJava
     }
 }
 
+class InterviewBit {
 
+    // Function to display all permutations of the string str
+    public static void printAllPer(String str, String str2)
+    {
+        // check if string is empty or null
+        if (str.length() == 0)
+        {
+            System.out.print(str2 + " ");
+            return;
+        }
+
+        for (int i = 0; i < str.length(); i++)
+        {
+            // ith character of str
+            char ch = str.charAt(i);
+            // Rest of the string after excluding
+            // the ith character
+            String str3 = str.substring(0, i) + str.substring(i + 1);
+            // Recursive call
+            printAllPer(str3, str2 + ch);
+        }
+    }
+    // Driver code
+    public static void main(String[] args)
+    {
+        String s = "cat";
+        printAllPer(s, "");
+    }
+}
+
+
+
+public CreateSingltonClass
+{
+
+
+
+}
 
 
 
