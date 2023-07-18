9fc79207efd5152be618c5c17e36f555d66cb2f3
Vivek pandey <vp1446392@gmail.com>
2023-07-18 22:55:54 +0530

    added sorting of integers

--- src/Main.java
+++ src/Main.java
@@ -1482,6 +1482,45 @@ class FibonacciSeriesWithRange
 
     }
 }
+class SortingOfIntegers
+{
+
+
+    public static void main(String[] args) {
+
+
+        int [] arr = {10,55,45,65,56,78,1,3,5,7};
+
+        for (int i = 0; i <arr.length; i++) {
+
+            for (int j = 0; j < arr.length-i-1 ; j++) {
+
+                  if(arr[j] > arr[j+1])
+                  {
+                      int temp = arr[j];
+                      arr[j] = arr[j+1];
+                      arr[j+1] = temp;
+                  }
+
+            }
+
+
+
+        }
+
+        for (Integer value: arr) {
+
+            System.out.println(value);
+
+        }
+
+
+
+
+    }
+
+}
+
 
 
 
