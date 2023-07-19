d731d5000a5ba4cf5478af2bd5e3d664e75ea2a6
Vivek Pandey <vp1446392@gmail.com>
2023-07-20 00:22:35 +0530

    added ReverseInteger program

--- src/Main.java
+++ src/Main.java
@@ -1737,6 +1737,28 @@ class MatchingElementsInArrays {
 }
 
 
+class ReverseNumber
+{
+
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        int number  = sc.nextInt();
+        int reverseNumber = 0;
+        while (number!=0)
+        {
+            int rem = number%10;
+            reverseNumber = reverseNumber*10+rem;
+            number /=10;
+        }
+        System.out.println("reverseNumber = " + reverseNumber);
+
+
+    }
+}
+
+
 
 
 
