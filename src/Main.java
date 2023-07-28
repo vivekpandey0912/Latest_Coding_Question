798199f83f74838de40da4e77f0a47748b6893fd
Vivek Pandey <vp1446392@gmail.com>
2023-07-28 11:43:27 +0530

    write a program to check PrimeNumbersWithStars

--- src/Main.java
+++ src/Main.java
@@ -2517,6 +2517,37 @@ class FindUnionInArray
 }
 
 
+public class PrimeNumbersWithStars {
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = { 2, 7, 4, 11, 8 };
+
+        System.out.println("Prime Numbers with Stars:");
+        for (int num : numbers) {
+            if (isPrime(num)) {
+                System.out.print(num + " ");
+                for (int i = 0; i < num; i++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
+
 
 
 
