ffb901f3eb1910cab86356c7b7b0b394b129b0c2
Vivek Pandey <vp1446392@gmail.com>
2023-08-04 23:46:59 +0530

    done with program TrailingZerosInFactorial

--- src/Main.java
+++ src/Main.java
@@ -3064,6 +3064,70 @@ class DiagonalSum {
     }
 }
 
+class MatrixTranspose {
+
+    public static int[][] transposeMatrix(int[][] matrix) {
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+
+        int[][] transpose = new int[columns][rows];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                transpose[j][i] = matrix[i][j];
+            }
+        }
+
+        return transpose;
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        System.out.println("Original Matrix:");
+        printMatrix(matrix);
+
+        int[][] transposeMatrix = transposeMatrix(matrix);
+
+        System.out.println("\nTranspose Matrix:");
+        printMatrix(transposeMatrix);
+    }
+}
+class TrailingZerosInFactorial {
+
+    public static int countTrailingZerosInFactorial(int number) {
+        int count = 0;
+        int divisor = 5;
+
+        while (number >= divisor) {
+            count += number / divisor;
+            divisor *= 5;
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int num = 25;
+        int trailingZeros = countTrailingZerosInFactorial(num);
+        System.out.println("Number of trailing zeros in " + num + " factorial: " + trailingZeros);
+    }
+}
+
+
 
 
 
