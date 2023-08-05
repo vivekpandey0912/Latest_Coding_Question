22b616572dce50a24cb02c8b877a4fcfc7af8442
Vivek Pandey <vp1446392@gmail.com>
2023-08-05 23:18:17 +0530

    find the number is strong or not

--- src/Main.java
+++ src/Main.java
@@ -3127,6 +3127,42 @@ class TrailingZerosInFactorial {
     }
 }
 
+class StrongNumber {
+
+    public static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+
+    public static boolean isStrongNumber(int num) {
+        int originalNum = num;
+        int sumOfFactorials = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            sumOfFactorials += factorial(digit);
+            num /= 10;
+        }
+
+        return originalNum == sumOfFactorials;
+    }
+
+    public static void main(String[] args) {
+        int number = 145;
+        if (isStrongNumber(number)) {
+            System.out.println(number + " is a Strong Number.");
+        } else {
+            System.out.println(number + " is not a Strong Number.");
+        }
+    }
+}
+
+
+
+
 
 
 
