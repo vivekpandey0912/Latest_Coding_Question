5d1c832aff7528a087f43a3b01d5b31bce4e88aa
Vivek Pandey <vp1446392@gmail.com>
2023-09-01 19:36:56 +0530

    done with pass by value

--- src/Main.java
+++ src/Main.java
@@ -3985,6 +3985,35 @@ class DeleteParticularElementFromArray
     }
 }
 
+// Call by Reference and Call By Value
+
+
+// Call By Value
+
+class CallByValue
+{
+
+
+    public static int checkAddition(int value1, int value2)
+    {
+        return value1+value2;
+    }
+
+    public static void main(String[] args) {
+
+        int value1 = 10;
+        int value2 = 20;
+
+        System.out.println("checkAddition(value1,value2) = " + checkAddition(value1, value2));
+        System.out.println("value1 = " + value1);
+        System.out.println("value2 = " + value2);
+
+    }
+}
+
+
+
+
 
 
 
