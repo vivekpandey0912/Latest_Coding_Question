1a961a67c603960c3badd6fad3d7913c070c1795
Vivek Pandey <vp1446392@gmail.com>
2023-09-04 16:45:52 +0530

    selection sort

--- src/Main.java
+++ src/Main.java
@@ -4298,7 +4298,47 @@ class InsertainSort
     }
 
 
+// Selection sort
+class SelectionSort {
 
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 9, 3, 1};
+        System.out.println("Original Array: ");
+        printArray(arr);
+
+        selectionSort(arr);
+
+        System.out.println("\nSorted Array: ");
+        printArray(arr);
+    }
+
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+
+            // Find the index of the minimum element in the remaining unsorted portion of the array
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // Swap the minimum element with the element at the current position
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
 
 
 
