ae1b34eaa1617220b59bf2de37f1355c3c792fbc
Vivek Pandey <vp1446392@gmail.com>
2023-09-14 10:42:20 +0530

    done with swapping with third variable

--- src/Main.java
+++ src/Main.java
@@ -5239,6 +5239,24 @@ class CreateThreadWithRunnableInterface implements Runnable
 class SwappingProgram
 {
 
+
+    public static void main(String[] args)
+    {
+        // Thrid Variable
+        int a  = 5;
+        int b = 10;
+        int c = a;
+        a = b;
+        b = c;
+        System.out.println(a);
+        System.out.println(b);
+
+
+
+
+
+
+    }
 }
 
 
