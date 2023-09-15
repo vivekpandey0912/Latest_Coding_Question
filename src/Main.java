79c83baac84ac77d3a0df95fcb9eb254a98bd310
Vivek Pandey <vp1446392@gmail.com>
2023-09-15 12:23:35 +0530

    done with remove whitespace from string

--- src/Main.java
+++ src/Main.java
@@ -5374,6 +5374,39 @@ class Persons implements Serializable {
 
 // Write program to remove whitespace from string
 
+class RemoveWhiteSpace
+{
+
+    public static void removeWhiteSpace(String str)
+    {
+        String str1 = "";
+        for (int i = 0; i <str.length(); i++) {
+            if(str.charAt(i)==' ')
+            {
+                str1 = str1;
+            }
+            else
+            {
+                str1 = str1+str.charAt(i);
+            }
+        }
+        System.out.println(str1);
+    }
+
+    public static void main(String[] args) {
+
+     String str1 = "vivek Kumar Pandey";
+        RemoveWhiteSpace.removeWhiteSpace(str1);
+
+
+
+    }
+
+
+
+
+}
+
 
 
 
