50955d26e106e7f5826a57472704aad2d64131c0
Vivek Pandey <vp1446392@gmail.com>
2023-09-15 14:10:41 +0530

    find the leap year

--- src/Main.java
+++ src/Main.java
@@ -5630,7 +5630,29 @@ class ArmStrongNumber10
 
 }
 
+class LeapYear1
+{
 
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter year");
+        int year = sc.nextInt();
+        if(year%4 == 0  && year%100 !=0 || year%400 ==100 )
+        {
+            System.out.println("The year is leap year");
+        }
+        else
+        {
+            System.out.println("This is not leap year");
+        }
+
+
+
+
+    }
+}
 
 
 
