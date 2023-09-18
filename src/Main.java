cb4c68bd07a92ab837364087ec7b71a41fb2cbfc
Vivek Pandey <vp1446392@gmail.com>
2023-09-18 16:34:50 +0530

    implementing comparator in java

--- src/Main.java
+++ src/Main.java
@@ -5683,10 +5683,91 @@ class OccuranceOfNumber
     }
 }
 
+// Implementing Comparator
+
+class StudentDetails1 implements Comparable<StudentDetails1>
+{
+    private String name;
+    private int age;
+    private String address;
+
+    public StudentDetails1() {
+    }
+
+    public StudentDetails1(String name, int age, String address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public int compareTo(StudentDetails1 o) {
+        return Integer.compare(this.age,o.getAge());
+    }
+}
+class SortByAddress implements Comparator<StudentDetails1>
+{
+
+    @Override
+    public int compare(StudentDetails1 o1, StudentDetails1 o2) {
+        return o1.getAddress().compareTo(o2.getAddress());
+    }
+}
+
+class StudentDetails1Impl
+{
+
+    public static void main(String[] args) {
+
+
+        List<StudentDetails1> list = new ArrayList<>();
+        list.add(new StudentDetails1("Vivek",25,"Delhi"));
+        list.add(new StudentDetails1("Vivek",22,"Hyd"));
+        list.add(new StudentDetails1("Vivek",21,"Mumbai"));
+        list.add(new StudentDetails1("Vivek",24,"MP"));
+        list.add(new StudentDetails1("Vivek",20,"UP"));
+        list.add(new StudentDetails1("Vivek",14,"Chennai"));
+
+        Collections.sort(list, new Comparator<StudentDetails1>() {
+            @Override
+            public int compare(StudentDetails1 o1, StudentDetails1 o2) {
+                return o1.getName().compareTo(o1.getName());
+            }
+        });
+        System.out.println("This is printing with stream sorting with name");
+        list.stream().forEach(System.out::println);
+        System.out.println("Sorting with name without stream ");
+        list.forEach(System.out::println);
 
 
 
+    }
 
+}
 
 
 
