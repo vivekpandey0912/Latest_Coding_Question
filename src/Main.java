2a0c47cbcb5af970bedde2f78e9275ac851452b6
Vivek Pandey <vp1446392@gmail.com>
2023-09-20 16:12:36 +0530

    done with sorting of bubble sort

--- src/Main.java
+++ src/Main.java
@@ -6041,7 +6041,35 @@ class StoreValueStartFromZero
 
 
 
+class SortArray
+{
 
+    public static void sortArray(int [] array)
+    {
+
+        for (int i = 0; i <array.length; i++) {
+            for (int j = 0; j <array.length-1; j++) {
+                if(array[j] > array[j+1])
+                {
+                   int temp = array[j];
+                   array[j] = array[j+1];
+                   array[j+1] = temp;
+                }
+            }
+        }
+        Arrays.stream(array).forEach(System.out::println);
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+        int [] array = {31,10,28,15,52,20};
+        sortArray(array);
+
+    }
+}
 
 
 
