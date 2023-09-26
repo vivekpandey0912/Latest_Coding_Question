2a86b361feff3e861bb42f900cf098cf3d98b0b0
Vivek Pandey <vp1446392@gmail.com>
2023-09-26 23:25:04 +0530

    done with second largest element

--- src/Main.java
+++ src/Main.java
@@ -6339,14 +6339,28 @@ class SecondLargestNumber
     {
         for (int i = 0; i <array.length; i++) {
 
+            for (int j = 0; j <array.length-1; j++) {
+
+                if(array[j]>array[j+1])
+                {
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+
+            }
+
         }
+        Arrays.stream(array).forEach(System.out::println);
+        System.out.println("The Second Largest Element is " + array[array.length-2]);
     }
 
 
 
     public static void main(String[] args) {
 
-
+        int [] array = {10,15,78,1,4,45,14,32,9,8,7};
+        findSecondLargest(array);
 
     }
 }
