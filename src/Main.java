fa1acec789fbfbba5986c7d5b9b689e73c18e633
Vivek Pandey <vp1446392@gmail.com>
2023-09-26 22:02:18 +0530

    adding program to count vowels in a word

--- src/Main.java
+++ src/Main.java
@@ -6210,6 +6210,39 @@ class HelloWorld{
   }
 }
 }
+// Write a program to count vowles in a word;
+class CountVowels
+{
+
+    public static void countVowels(String str)
+    {
+        int count = 0;
+
+        for (int i = 0; i <str.length(); i++) {
+
+            if(str.charAt(i)=='a' || str.charAt(i)=='e' || str.charAt(i)=='i'
+                    || str.charAt(i)=='o' || || str.charAt(i)=='u')
+            {
+                count = count+1;
+
+            }
+
+        }
+        System.out.println("The Total Vowels in words is " + count);
+
+    }
+
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+}
+
+
 
 
 
