924053dcdf2285c3111d079ee46a18cc9fc1063f
Vivek Pandey <vp1446392@gmail.com>
2023-09-29 16:34:14 +0530

    done with find duplicate number between two array

--- src/Main.java
+++ src/Main.java
@@ -6410,12 +6410,40 @@ class RemoveElement
     }
 }
 
+abstract class GetValue
+{
+
+}
+
+class FindCommonElementBetweenTwoArray
+{
+  public static void findCommonElementBetweenTwoArray(int [] array1, int [] array2)
+  {
+      Set<Integer> set1 = new HashSet<>();
+      Set<Integer> set2 = new HashSet<>();
+
+      for (int num : array1) {
+          set1.add(num);
+      }
+
+      for (int num : array2) {
+          set2.add(num);
+      }
+
+      set1.retainAll(set2);
+
+      System.out.println("Common elements: " + set1);
+  }
+
+    public static void main(String[] args) {
+        int[] array1 = {1, 2, 3, 4, 5};
+        int[] array2 = {4, 5, 6, 7, 8};
+        findCommonElementBetweenTwoArray(array2,array1);
 
 
 
-
-
-
+    }
+}
 
 
 
