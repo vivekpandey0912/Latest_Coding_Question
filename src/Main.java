651372d0d0dff1f5463270afc821dc2572c32db9
Vivek Pandey <vp1446392@gmail.com>
2023-10-11 10:40:26 +0530

    write program to get perfect number

--- src/Main.java
+++ src/Main.java
@@ -6886,6 +6886,41 @@ class PalindromString
     }
 
 }
+// Perfect Number
+
+class PerfectNumber {
+
+    public static boolean isPerfectNumber(int num) {
+        if (num < 2) {
+            return false;
+        }
+
+        int divisorsSum = 1;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                divisorsSum += i;
+                if (i != num / i) {
+                    divisorsSum += num / i;
+                }
+            }
+        }
+
+        return divisorsSum == num;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+
+        if (isPerfectNumber(num)) {
+            System.out.println(num + " is a perfect number");
+        } else {
+            System.out.println(num + " is not a perfect number");
+        }
+    }
+}
 
 
 
